956b3ca748cb8e36258e063edf0b99865625a2c2
natu <natnaeldarsema.com>
2025-02-14 15:22:55 +0300

    commit other components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,14 +1,18 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
+// import WelcomeMessage from './components/WelcomeMessage'
+import MainContent from './components/MainContent'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App() {
 
   return (
     <>
-      <WelcomeMessage />
+     
+      <Header />
+      <MainContent />
+      <Footer />
     </>
   )
 }
