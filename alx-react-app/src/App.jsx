78b3074d743f5db0f2884cb0d0defed4079841d7
natu <natnaeldarsema.com>
2025-02-21 14:05:05 +0300

    update the app.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import './App.css'
 import UserProfile from './components/UserProfile';
-// import WelcomeMessage from './components/WelcomeMessage'
+import WelcomeMessage from './components/WelcomeMessage'
 import MainContent from './components/MainContent'
 import Header from './components/Header'
 import Footer from './components/Footer'
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <>
+    <WelcomeMessage />
      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
      
