c893d0e9a3c4e7b3c298a9ca83d85b7505190a3b
natu <natnaeldarsema.com>
2025-03-15 01:42:55 +0300

    copied file

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -5,16 +5,20 @@ import WelcomeMessage from './components/WelcomeMessage'
 import MainContent from './components/MainContent'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import Counter from './components/Counter';
 
 function App() {
 
   return (
     <>
-    <WelcomeMessage />
-     <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-
+    <Header />
+    {/* <WelcomeMessage /> */}
+     {/* <UserProfile name="Alice" age="25" bio="Loves hiking and photography" /> */}
+     <MainContent />
+      <Counter />
+      <Footer />
      
-      {/* <Header />
+      {/* 
       <MainContent />
       <Footer /> */}
     </>
