9e47a43ea6424ef130945a8724f29a0cc1cd7737
natu <natnaeldarsema.com>
2025-04-06 23:58:27 +0300

    alx week 12 1st task

--- form-handling-react/src/components/RegistrationForm.jsx
+++ form-handling-react/src/components/RegistrationForm.jsx
@@ -36,21 +36,21 @@ const RegistrationForm = () => {
         type="text"
         name="username"
         placeholder="Username"
-        value={formData.username}
+        value={formData.username} // Set value for username
         onChange={handleChange}
       />
       <input
         type="email"
         name="email"
         placeholder="Email"
-        value={formData.email}
+        value={formData.email} // Set value for email
         onChange={handleChange}
       />
       <input
         type="password"
         name="password"
         placeholder="Password"
-        value={formData.password}
+        value={formData.password} // Set value for password
         onChange={handleChange}
       />
       <button type="submit">Register</button>
