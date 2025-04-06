9d989a502578217c7ba4f27a647d8ec3323150c0
natu <natnaeldarsema.com>
2025-04-06 18:59:26 +0300

    alx weeek 10

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,7 +2,12 @@ import { Link } from 'react-router-dom';
 
 function Navbar() {
   return (
-    <nav style={{ padding: '10px', background: '#f0f0f0' }}>
+    <nav style={{
+      backgroundColor: '#f0f0f0',
+      display: 'flex',
+      justifyContent: 'space-around',
+      padding: '10px'
+    }}>
       <Link to="/" style={{ margin: '10px' }}>Home</Link>
       <Link to="/about" style={{ margin: '10px' }}>About</Link>
       <Link to="/services" style={{ margin: '10px' }}>Services</Link>
